e698c92f8d64ed8f8878358b2b078fdbea9da0c1
Fawl3r <Fawl3r@gmail.com>
2023-10-20 10:39:45 -0500

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import router from "next/router";
 import { motion } from "framer-motion";
 import { FaDiscord, FaYoutube } from "react-icons/fa";
 import { RiTwitterXFill } from "react-icons/ri";
@@ -11,6 +12,7 @@ import { AiOutlineClose } from "react-icons/ai";
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobileF3DropdownOpen, setIsMobileF3DropdownOpen] = useState(false);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -22,6 +24,17 @@ const Navbar = () => {
 
   const handleMouseLeave = () => {
     setIsDropdownOpen(false);
+  };
+  const toggleMobileF3Dropdown = () => {
+    setIsMobileF3DropdownOpen(!isMobileF3DropdownOpen);
+  };
+  const scrollToContent = (elementId) => {
+    setIsMobileMenuOpen(false);
+
+    const element = document.querySelector(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   useEffect(() => {
@@ -37,6 +50,9 @@ const Navbar = () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+  const mobileStyles = `fixed top-20 right-0 z-50 ${
+    isMobileF3DropdownOpen ? "h-4/5" : "h-3/5"
+  } w-3/5 bg-gradient-to-b from-black to-slate-800 p-4`;
 
   return (
     <motion.nav
@@ -130,7 +146,7 @@ const Navbar = () => {
           <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
             <Link
               href="https://gods-of-gaming.gitbook.io/gods-of-gaming-whitepaper/"
-              target="_blank"  // Add this if you want the link to open in a new tab
+              target="_blank"
               className="transition duration-500 ease-in-out hover:text-blue-400"
             >
               Whitepaper
@@ -197,42 +213,105 @@ const Navbar = () => {
             )}
           </button>
         </div>
+        {/* className="fixed top-20 right-0 z-50 w-3/5 h-3/5 bg-gradient-to-b from-black to-slate-800 p-4 " */}
         {isMobileMenuOpen && (
           <motion.div
-            className="fixed top-20 right-0 z-50 w-3/5 h-3/5 bg-gradient-to-b from-black to-slate-800 p-4 "
+            className={mobileStyles}
             initial={{ opacity: 0, x: "100%" }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: "100%", transition: { duration: 0.8 } }}
             transition={{ duration: 0.6 }}
           >
             <div className="flex flex-col space-y-7 text-white items-center m-4">
-              <Link href="#nft" className="text-2xl">
-                NFT Artwork
+              <Link
+                href="#nft"
+                className="text-2xl"
+                onClick={() => scrollToContent("#nftart")}
+              >
+                <span>NFT Artwork</span>
               </Link>
-              <Link href="#F3" className="text-2xl">
-                F3 Dominion
-              </Link>
-              <Link href="https://f3-packs.vercel.app/" className="text-2xl">
+              <div className="relative">
+                <span
+                  onClick={toggleMobileF3Dropdown}
+                  className="cursor-pointer text-2xl hover:text-blue-400"
+                >
+                  F3 Dominion
+                </span>
+                {isMobileF3DropdownOpen && (
+                  <div
+                    className=" text-white p-2 shadow-md transition duration-500 ease-in-out z-50"
+                    style={{ minWidth: "280px", width: "auto" }}
+                  >
+                    <div className="space-y-3 flex flex-col w-full items-center m-2">
+                      <Link
+                        className="cursor-pointer transition duration-500 ease-in-out  hover:text-blue-400"
+                        href="https://f3-nexium-marketplace.vercel.app/"
+                        target="_blank"
+                      >
+                        Nexium NFT Marketplace
+                      </Link>
+                      <Link
+                        className="cursor-pointer transition duration-500 ease-in-out  hover:text-blue-400"
+                        href="https://f3-limited-edition.vercel.app/"
+                        target="_blank"
+                      >
+                        Limited Edition NFT Marketplace
+                      </Link>
+                      <Link
+                        className="cursor-pointer transition duration-500 ease-in-out  hover:text-blue-400 "
+                        href="https://f3-liveraffle.vercel.app/"
+                        target="_blank"
+                      >
+                        Raffle Machine #1
+                      </Link>
+                      <Link
+                        className="cursor-pointer transition duration-500 ease-in-out  hover:text-blue-400"
+                        href="https://f3-liveraffle.vercel.app/"
+                        target="_blank"
+                      >
+                        Raffle Machine #2
+                      </Link>
+                    </div>
+                  </div>
+                )}
+              </div>
+              <Link
+                href="https://f3-packs.vercel.app/"
+                className="text-2xl"
+                target="_blank"
+              >
                 F3 Packs
               </Link>
-              <Link href="#white" className="text-2xl">
+              <Link
+                href="#white"
+                className="text-2xl"
+                onClick={() => scrollToContent("#white")}
+              >
                 Whitepaper
               </Link>
 
-              {/* Add more mobile menu links here */}
               <div className="flex space-x-4 py-8">
-                <FaYoutube
-                  className="cursor-pointer transition duration-500 ease-in-out hover:bg-red-600"
-                  size={30}
-                />
-                <FaDiscord
-                  className="cursor-pointer transition duration-500 ease-in-out"
-                  size={30}
-                />
-                <RiTwitterXFill
-                  className="cursor-pointer transition duration-500 ease-in-out"
-                  size={30}
-                />
+                <Link href="https://www.youtube.com/@Fawl3r" target="_blank">
+                  <FaYoutube
+                    className="cursor-pointer transition duration-700 ease-in-out "
+                    size={30}
+                  />
+                </Link>
+                <Link
+                  href="https://discord.com/invite/67ZVXVQSfX"
+                  target="_blank"
+                >
+                  <FaDiscord
+                    className="cursor-pointer transition duration-700 ease-in-out hover:text-blue-500"
+                    size={30}
+                  />
+                </Link>
+                <Link href="https://twitter.com/GOG__Official_" target="_blank">
+                  <RiTwitterXFill
+                    className="cursor-pointer transition duration-700 ease-in-out hover:text-blue-500"
+                    size={30}
+                  />
+                </Link>
               </div>
             </div>
           </motion.div>
